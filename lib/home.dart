3931468984722ee5f170662f6d948d011fc44cc5
Damodar Lohani <dlohani48@gmail.com>
2024-02-15 10:28:13 +0545

    using gemini

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+import 'config.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,7 +13,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _inputController = TextEditingController();
-  final List<String> chats = [];
+  late final ChatSession _session;
+  final GenerativeModel _model =
+      GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _session = _model.startChat();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,37 +32,95 @@ class _HomeState extends State<Home> {
           child: ListView(
             padding: const EdgeInsets.all(16.0),
             children: [
-              ...chats.map(
-                (chat) => Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(chat),
-                    const SizedBox(height: 10.0),
-                  ],
-                ),
+              ..._session.history.map(
+                (content) {
+                  var text = content.parts
+                      .whereType<TextPart>()
+                      .map<String>((e) => e.text)
+                      .join('');
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        content.role == 'user' ? 'User:' : 'Gemini:',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      MarkdownBody(data: text),
+                      const SizedBox(height: 10.0),
+                    ],
+                  );
+                },
               ),
             ],
           ),
         ),
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _inputController,
-                decoration: const InputDecoration(
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _inputController,
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    helperText: 'Enter your prompt'),
+                    hintText: 'Enter your prompt',
+                  ),
+                ),
               ),
-            ),
-            IconButton(
-              onPressed: () {
-                debugPrint(_inputController.text);
-              },
-              icon: const Icon(Icons.send),
-            )
-          ],
+              _loading
+                  ? const CircularProgressIndicator()
+                  : IconButton(
+                      onPressed: _sendMessage,
+                      icon: const Icon(Icons.send),
+                    ),
+            ],
+          ),
         )
       ]),
     );
+  }
+
+  _sendMessage() async {
+    setState(() {
+      _loading = true;
+    });
+    try {
+      final response =
+          await _session.sendMessage(Content.text(_inputController.text));
+      var text = response.text;
+
+      if (text == null) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('No response from API')));
+        }
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _inputController.clear();
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  _showError(String message) {
+    if (context.mounted) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(message)));
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,4 +15,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
